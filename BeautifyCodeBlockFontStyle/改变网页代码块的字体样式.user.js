e2398a28ac1cf120169f9de2d7f2b93cc65abb0f
LiarCoder <liarwhen@qq.com>
2021-09-24 17:04:13 +0800

    3号脚本：将LeetCode的问题描述区文字样式也做了修改

--- BeautifyCodeBlockFontStyle/改变网页代码块的字体样式.user.js
+++ BeautifyCodeBlockFontStyle/改变网页代码块的字体样式.user.js
@@ -4,12 +4,12 @@
  * @Author: LiarCoder
  * @Date: 2021-09-21 00:11:42
  * @LastEditors: LiarCoder
- * @LastEditTime: 2021-09-24 02:07:51
+ * @LastEditTime: 2021-09-24 17:02:06
  */
 // ==UserScript==
 // @name         改变网页代码块的字体样式
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  改变网页代码块的字体样式，LeetCode、CSDN、掘金、博客园的代码块字体全被设置为Cascadia Code这款字体，后面还有YaHei Consolas Hybrid和Lucida Console这两款字体做候选，最后由CSDN代码块的字体样式兜底
 // @author       LiarCoder
 // @match        *://*/*
@@ -23,7 +23,8 @@
   let CSDNCodeBlockOriginalFont = 'Source Code Pro,DejaVu Sans Mono,Ubuntu Mono,Anonymous Pro,Droid Sans Mono,Menlo,Monaco,Consolas,Inconsolata,Courier,monospace,PingFang SC,Microsoft YaHei,sans-serif';
   let CSDNCodeBlockSelector = 'span.token, .prism-atom-one-dark .prism *';
   let cnblogCodeBlockSelector = '.syntaxhighlighter a, .syntaxhighlighter div, .syntaxhighlighter code, .syntaxhighlighter table, .syntaxhighlighter table td, .syntaxhighlighter table tr, .syntaxhighlighter table tbody, .syntaxhighlighter table thead, .syntaxhighlighter table caption, .syntaxhighlighter textarea';
-  codeBlockStyle.innerText = `${CSDNCodeBlockSelector}, ${cnblogCodeBlockSelector}, blockquote, code{
+  let leetcodeDescriptionBlockSelector = '.notranslate, pre';
+  codeBlockStyle.innerText = `${CSDNCodeBlockSelector}, ${cnblogCodeBlockSelector}, ${leetcodeDescriptionBlockSelector}, blockquote, code{
     font-family: 'Cascadia Code', 'YaHei Consolas Hybrid', 'Lucida Console', ${CSDNCodeBlockOriginalFont} !important;
   }
   div.view-line>span>span.mtk3{
