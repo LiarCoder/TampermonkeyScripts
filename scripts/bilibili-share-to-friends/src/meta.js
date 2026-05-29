fe0bfbfc6f2e795982d0da4415735ecec5bb5e24
LiarCoder <liarwhen@qq.com>
2026-05-29 18:04:00 +0800

    feat: 添加 Bilibili 脚本图标元信息

--- scripts/bilibili-share-to-friends/src/meta.js
+++ scripts/bilibili-share-to-friends/src/meta.js
@@ -5,6 +5,7 @@ export const userscript = {
   description:
     "在 Bilibili 视频播放页的原分享面板中新增“B站好友”入口，将当前视频以私信分享卡片发送给最近私信联系人",
   author: "LiarCoder",
+  icon: "https://www.bilibili.com/favicon.ico",
   match: ["https://www.bilibili.com/video/*"],
   grant: ["GM_addStyle", "GM_xmlhttpRequest"],
   connect: ["api.bilibili.com", "api.vc.bilibili.com"],
