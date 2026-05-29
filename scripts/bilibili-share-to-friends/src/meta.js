04664021f75f2ef13c074df24fc9ac80553ada2e
LiarCoder <liarwhen@qq.com>
2026-05-29 22:51:51 +0800

    docs: bilibili-share-to-friends 更新好友来源功能说明

--- scripts/bilibili-share-to-friends/src/meta.js
+++ scripts/bilibili-share-to-friends/src/meta.js
@@ -1,9 +1,9 @@
 export const userscript = {
   name: "B站视频分享给好友",
   namespace: "http://tampermonkey.net/",
-  version: "0.2.0",
+  version: "0.3.0",
   description:
-    "在 Bilibili 视频播放页的原分享面板中新增“B站好友”入口，将当前视频以私信分享卡片发送给最近私信联系人",
+    "在 Bilibili 视频播放页的原分享面板中新增“B站好友”入口，将当前视频以文本链接私信发送给最近聊天、关注或粉丝用户",
   author: "LiarCoder",
   icon: "https://www.bilibili.com/favicon.ico",
   match: ["https://www.bilibili.com/video/*"],
